6565d917275a4861bdc9c192c50569cc9fbeefcf
Bilokon <bilokon.evgeniy@gmail.com>
2025-12-11 15:02:45 +0200

    web test route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// route for tests DO NOT COMMIT CHANGES!
+Route::get('/v1/test', App\Http\Controllers\Api\ApiTestController::class)->name('api_test');
+
 Route::get('/', function () {
     return view('welcome');
 });
