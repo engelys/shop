3a83c61a39821c5753b532a561bd36803496a9c7
Bilokon <bilokon.evgeniy@gmail.com>
2025-12-11 15:18:14 +0200

    update collection requests use params dto

--- app/Http/Integrations/WP/Requests/WPProducts.php
+++ app/Http/Integrations/WP/Requests/WPProducts.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Integrations\WP\Requests;
 
+use App\Http\Integrations\WP\Params\CollectionParams;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 
@@ -10,18 +11,14 @@ class WPProducts extends Request
     protected Method $method = Method::GET;
 
     public function __construct(
-        protected int $page = 1,
-        protected int $per_page = 10,
+        protected CollectionParams $params
     )
     {
     }
 
     protected function defaultQuery(): array
     {
-        return [
-            'page' => $this->page,
-            'per_page' => $this->per_page,
-        ];
+        return $this->params->all();
     }
 
     public function resolveEndpoint(): string
