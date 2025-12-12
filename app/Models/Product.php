b25f6b6c77cabb1ba2234936131aa4ead1478611
Bilokon <bilokon.evgeniy@gmail.com>
2025-12-12 20:02:47 +0200

    Product upsells, related, crosssells, grouped

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -27,8 +27,33 @@ class Product extends Model
         return $this->belongsToMany(Category::class, 'product_categories');
     }
 
+//    public function attributes(): BelongsToMany
+//    {
+//        return $this->belongsToMany(Attribute::class, 'product_attributes');
+//    }
+
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'product_tags');
+    }
+
+    public function upsells(): BelongsToMany
+    {
+        return $this->belongsToMany(ProductUpsell::class, 'product_upsells', 'product_id', 'upsell_product_id');
+    }
+
+    public function related(): BelongsToMany
+    {
+        return $this->belongsToMany(ProductRelated::class, 'product_related', 'product_id', 'related_product_id');
+    }
+
+    public function crosssells(): BelongsToMany
+    {
+        return $this->belongsToMany(ProductCrosssell::class, 'product_cross_sells', 'product_id', 'cross_sell_product_id');
+    }
+
+    public function grouped(): BelongsToMany
+    {
+        return $this->belongsToMany(ProductGrouped::class, 'product_grouped', 'product_id', 'child_product_id');
     }
 }
---
+++ app/Models/ProductCrosssell.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCrosssell extends Model
+{
+    protected $table = 'product_cross_sells';
+
+    public $timestamps = false;
+}
