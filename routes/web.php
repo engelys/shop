2f939cba7db32e84a9c7ef8fe8309c65e74d196e
Bilokon <bilokon.evgeniy@gmail.com>
2025-12-15 21:06:29 +0200

    base shop web routes, pages, ShopController

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 // route for tests DO NOT COMMIT CHANGES!
 Route::get('/v1/test', App\Http\Controllers\Api\ApiTestController::class)->name('api_test');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# main page
+Route::get('/', [ShopController::class, 'index'])->name('shop.index');
+# product page
+Route::get('/product/{product:slug}', [ShopController::class, 'show'])->name('shop.products.show');
+# categories list
+Route::get('/shop/', [ShopController::class, 'categories'])->name('shop.categories.categories');
+# category products
+Route::get('/shop/categories/{category:slug}', [ShopController::class, 'category'])->name('shop.categories.category');
